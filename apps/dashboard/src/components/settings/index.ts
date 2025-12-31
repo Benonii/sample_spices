dc049eb6563a827acae5c86a2bde9526101bfe0b
BENONI ESCKINDER <besckinder@gmail.com>
2025-12-31 19:52:21 +0300

    chore: Removed dashbaord + Docker setup for front and backend

--- apps/dashboard/src/components/settings/index.ts
+++
@@ -1,4 +0,0 @@
-export { TwoFactorAuth } from './TwoFactorAuth';
-export { PasswordManagement } from './PasswordManagement';
-export { Passkeys } from './Passkeys';
-export { SessionManagement } from './SessionManagement'; 
--- apps/dashboard/src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./authClient";
-export * from "./consts";
-export * from "./generateFilter";
-export * from "./generateSort";
-export * from "./axios";
